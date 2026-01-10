4737f646c2d4e209174c87b56e33fd2f0b5a520a
Archisha <archisha1509@gmail.com>
2026-01-10 19:07:27 +0000

    io and main update

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from src.io import load_story, load_backstory
+from src.io import load_story, load_backstory, load_dataset, save_result, calc_accuracy
 from src.pipeline import run_pipeline
+import os
 
 def main():
-    story = load_story("data/story_001.txt")
-    backstory = load_backstory("data/backstory_001.txt")
+    #story = load_story("data/story_001.txt")
+    #backstory = load_backstory("data/backstory_001.txt")
+    if os.path.exists("outputs/results.csv"):
+        os.remove("outputs/results.csv")
 
-    label, rationale = run_pipeline(story, backstory)
+    if os.path.exists("data/train.csv"):
+        df=load_dataset("data/train.csv")
+    else:
+        raise FileNotFoundError("train.csv")
+    
+    story_cache = {}
+    
+    for row in df.itertuples(index=False):
+        bkname = row.book_name.strip().lower()
+        if bkname not in story_cache:
+            if os.path.exists(f"data/story/{bkname}.txt"):
+                story_cache[bkname] = load_story(f"data/story/{bkname}.txt")
+            else:
+                raise FileNotFoundError(bkname)
+        story = story_cache[bkname]
+        backstory=row.content
+        label, rationale = run_pipeline(story, backstory)
+        save_result(row.id,label,rationale)
 
-    print("Prediction:", label)
-    print("Rationale:", rationale)
+    print("Accuracy: ",calc_accuracy("data/train.csv","outputs/results.csv"))
+
+    #print("Prediction:", label)
+    #print("Rationale:", rationale)
 
 if __name__ == "__main__":
     main()
--- src/io.py
+++ src/io.py
@@ -1,4 +1,9 @@
+import pandas as pd
 # src/io.py
+
+def load_dataset(path):
+    df=pd.read_csv(path)
+    return df
 
 def load_story(path):
     with open(path, "r", encoding="utf-8") as f:
@@ -8,7 +13,7 @@ def load_backstory(path):
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
-def save_result(story_id, label, rationale, out_path="results.csv"):
+def save_result(story_id, label, rationale, out_path="outputs/results.csv"):
     import csv
     header = ["story_id", "prediction", "rationale"]
     row = [story_id, label, rationale]
@@ -22,3 +27,14 @@ def save_result(story_id, label, rationale, out_path="results.csv"):
         with open(out_path, "a", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow(row)
+
+def calc_accuracy(path_train,path_res):
+    df1,df2=load_dataset(path_train),load_dataset(path_res)
+    df_merged = df1.merge(df2, left_on="id", right_on="story_id")
+    label={"consistent":1,"contradict":0}
+    accuracy=0
+    for idx,row in df_merged.iterrows():
+        if label[row["label"].strip().lower()]==row["prediction"]:
+            accuracy+=1
+    return accuracy/len(df_merged)
+        
